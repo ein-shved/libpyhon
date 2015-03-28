66e82787c9bf280da5f4f1e6562eff0cd751d9f8
Shvedov Yury <shved@lvk.cs.msu.su>
2015-03-28 11:37:20 +0300

    Add newline end end of usage

--- str.c
+++ str.c
@@ -7,7 +7,7 @@ int main(int argc, char *argv[]) {
 
   if(argc<5) {
     fprintf(stderr,"Usage: <string> <start> <end> <repeat>\n\n\
-Print string[start:end]*repeat");
+Print string[start:end]*repeat\n");
     exit(0);
   }
   s = atoi(argv[2]);
